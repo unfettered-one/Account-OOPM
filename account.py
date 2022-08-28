35a72fa765c92ffb1dc141a730ffbe4b978f3070
unfettered-one <103358367+unfettered-one@users.noreply.github.com>
2022-08-28 19:43:31 +0530

    Update account.py

--- account.py
+++ account.py
@@ -1,29 +1,37 @@
+# creating class for account and creating methods
+
 class Account:
     """Creating a simple Account"""
 
     def __init__(self, name, balance):
         self.name = name
-        self.balance = balance
+        self.__balance = balance
         print('Account created for {0}.'.format(self.name))
+        self.showBalance()
 
     def deposit(self, amount):
         if amount > 0:
-            self.balance += amount
+            self.__balance += amount
             self.showBalance()
     def withdrawal(self, amount):
-        if amount > 0 and self.balance-amount > 0:
-            self.balance -= amount
+        if amount > 0 and self.__balance-amount > 0:
+            self.__balance -= amount
         else:
-            print('Amount is not valid in reelation to your balance.')
+            print('Amount is not valid in relation to your balance.')
         self.showBalance()
     
     def showBalance(self):
-        print(f'Your Account balance is {self.balance}.')
+        print(f'Your Account balance is {self.__balance}.')
 
 if __name__ == '__main__':
-    yash=Account('Yash', 0)
-    yash.showBalance()
+    yash=Account('Yash', 50)
+    # yash.showBalance()
     
     yash.deposit(6000)
-    yash.withdrawal(50000)
-    # yash.showBalance()  
+    yash.withdrawal(5000)
+    # yash.showBalance()    
+
+    yug=Account('Yug',0)
+    yug.__balance=200
+    yug.deposit(300)
+    yug.withdrawal(100)
